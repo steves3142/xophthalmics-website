00b288f7ef1614448627f7f78ceb7af36760f2a1
steves3142 <steves3142@gmail.com>
2022-11-19 01:28:44 -0500

    adding serif font

--- xophthalmics-website-1.0/components/Header.tsx
+++ xophthalmics-website-1.0/components/Header.tsx
@@ -5,11 +5,12 @@ type Props = {};
 function Header({}: Props) {
   return (
     <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
-      <p>Solutions</p>
-      <p>News</p>
-      <p>About Us</p>
-      <p>Contact</p>
-      <p>Buy</p>
+
+      <p className="font-serif">Solutions</p>
+      <p className="font-serif">News</p>
+      <p className="font-serif">About Us</p>
+      <p className="font-serif">Contact</p>
+      <p className="font-serif">Buy</p>
 
     </header>
   )
