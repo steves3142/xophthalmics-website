da05ebcf646b11ac56e7f2af3df483a122905c54
steves3142 <steves3142@gmail.com>
2022-11-19 00:41:25 -0500

    Add titles of nav bar, replaced vercel icon with xenon logo

--- xophthalmics-website-1.0/components/Header.tsx
+++ xophthalmics-website-1.0/components/Header.tsx
@@ -4,7 +4,14 @@ type Props = {};
 
 function Header({}: Props) {
   return (
-    <div>Header</div>
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+      <p>Solutions</p>
+      <p>News</p>
+      <p>About Us</p>
+      <p>Contact</p>
+      <p>Buy</p>
+
+    </header>
   )
 }
 
