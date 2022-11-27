040556df82fc4ef9b9a72e20a5c05e4b3f9454c0
steves3142 <steves3142@gmail.com>
2022-11-27 18:29:18 -0500

    changing github icon for LinkedIn icon

--- xophthalmics-website-1.0/components/Header.js
+++ xophthalmics-website-1.0/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Fragment } from 'react'
-import Link from "next/link"; 
+import Link from "next/link";
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline"
 
 const navigation = [
     { name: 'Solutions', href: `${process.env.NEXT_PUBLIC_BASE_URL}/solutions`, current: false },
-    { name: 'News', href: `${process.env.NEXT_PUBLIC_BASE_URL}/news`, current: true },
+    { name: 'News', href: `${process.env.NEXT_PUBLIC_BASE_URL}/news`, current: false },
     { name: 'About Us', href: `${process.env.NEXT_PUBLIC_BASE_URL}/about-us`, current: false },
     { name: 'Contact', href: `${process.env.NEXT_PUBLIC_BASE_URL}/contact`, current: false },
     { name: 'Buy', href: `${process.env.NEXT_PUBLIC_BASE_URL}/buy`, current: false },
@@ -69,8 +69,8 @@ function Header() {
                                 {/* Profile dropdown */}
                                 <Menu as="div" className="relative ml-3">
                                     <div>
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" className="w-6 h-6">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
                                         </svg>
                                     </div>
                                     <Transition
