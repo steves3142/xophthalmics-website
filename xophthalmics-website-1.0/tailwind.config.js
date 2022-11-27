90003b7386cfb301aa6fe866fa1aa040c16878b4
steves3142 <steves3142@gmail.com>
2022-11-26 23:00:14 -0500

    yarn add -D @tailwindcss/forms

--- xophthalmics-website-1.0/tailwind.config.js
+++ xophthalmics-website-1.0/tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}", 
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'warm-gray': colors.warmGray,
+        teal: colors.teal,
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
