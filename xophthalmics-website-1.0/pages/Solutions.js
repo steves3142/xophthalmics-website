3c1f51ebda41c435f25db5a4f57f2e987feb602b
steves3142 <steves3142@gmail.com>
2022-11-27 15:44:50 -0500

    yarn add -D @tailwindcss/typography
    Adding content to Solutions.js

--- xophthalmics-website-1.0/pages/Solutions.js
+++ xophthalmics-website-1.0/pages/Solutions.js
@@ -1,15 +1,302 @@
 import React from 'react'
 import Link from 'next/link'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import { CameraIcon } from '@heroicons/react/20/solid'
+
+const stats = [
+  { label: 'Founded', value: '2021' },
+  { label: 'Employees', value: '5' },
+  { label: 'Beta Users', value: '521' },
+  { label: 'Raised', value: '$25M' },
+]
 
 export default function Solutions() {
   return (
     <div>
-        Solutions
-        <h2>
-            <Link href="/">Back to Home</Link>
-        </h2>
+      Solutions
+      <h2>
+        <Link href="/">Back to Home</Link>
+      </h2>
+
+      <Header />
+
+      <div className="relative bg-white">
+        <div className="lg:absolute lg:inset-0">
+          <div className="lg:absolute lg:inset-y-0 lg:left-0 lg:w-1/2">
+            <img
+              className="h-56 w-full object-cover lg:absolute lg:h-full"
+              src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="relative px-4 pt-12 pb-16 sm:px-6 sm:pt-16 lg:mx-auto lg:grid lg:max-w-7xl lg:grid-cols-2 lg:px-8">
+          <div className="lg:col-start-2 lg:pl-8">
+            <div className="mx-auto max-w-prose text-base lg:ml-auto lg:mr-0 lg:max-w-lg">
+              <h2 className="font-semibold leading-6 text-indigo-600">Work with us</h2>
+              <h3 className="mt-2 text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">Our Process</h3>
+              <p className="mt-8 text-lg text-gray-500">
+                Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique
+                pellentesque. Blandit amet, sed aenean erat arcu morbi.
+              </p>
+              <div className="prose prose-indigo mt-5 text-gray-500">
+                <p>
+                  Sollicitudin tristique eros erat odio sed vitae, consequat turpis elementum. Lorem nibh vel, eget
+                  pretium arcu vitae. Eros eu viverra donec ut volutpat donec laoreet quam urna.
+                </p>
+                <p>
+                  Bibendum eu nulla feugiat justo, elit adipiscing. Ut tristique sit nisi lorem pulvinar. Urna, laoreet
+                  fusce nibh leo. Dictum et et et sit. Faucibus sed non gravida lectus dignissim imperdiet a.
+                </p>
+                <p>
+                  Dictum magnis risus phasellus vitae quam morbi. Quis lorem lorem arcu, metus, egestas netus cursus. In.
+                </p>
+                <ul role="list">
+                  <li>Quis elit egestas venenatis mattis dignissim.</li>
+                  <li>Cras cras lobortis vitae vivamus ultricies facilisis tempus.</li>
+                  <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
+                </ul>
+                <p>
+                  Rhoncus nisl, libero egestas diam fermentum dui. At quis tincidunt vel ultricies. Vulputate aliquet
+                  velit faucibus semper. Pellentesque in venenatis vestibulum consectetur nibh id. In id ut tempus
+                  egestas. Enim sit aliquam nec, a. Morbi enim fermentum lacus in. Viverra.
+                </p>
+                <h3>How we’re different</h3>
+                <p>
+                  Tincidunt integer commodo, cursus etiam aliquam neque, et. Consectetur pretium in volutpat, diam.
+                  Montes, magna cursus nulla feugiat dignissim id lobortis amet. Laoreet sem est phasellus eu proin massa,
+                  lectus. Diam rutrum posuere donec ultricies non morbi. Mi a platea auctor mi.
+                </p>
+                <p>
+                  Mauris ullamcorper imperdiet nec egestas mi quis quam ante vulputate. Vel faucibus adipiscing lacus,
+                  eget. Nunc fermentum id tellus donec. Ut metus odio sit sit varius non nunc orci. Eu, mi neque, ornare
+                  suspendisse amet, nibh. Facilisi volutpat lectus id sapien dis mauris rhoncus. Est rhoncus, interdum
+                  imperdiet ac eros, diam mauris, tortor. Risus id sit molestie magna.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="overflow-hidden bg-white">
+        <div className="relative mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:px-8">
+          <div className="absolute top-0 bottom-0 left-3/4 hidden w-screen bg-gray-50 lg:block" />
+          <div className="mx-auto max-w-prose text-base lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-8">
+            <div>
+              <h2 className="text-lg font-semibold text-indigo-600">Case Study</h2>
+              <h3 className="mt-2 text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">Meet Whitney</h3>
+            </div>
+          </div>
+          <div className="mt-8 lg:grid lg:grid-cols-2 lg:gap-8">
+            <div className="relative lg:col-start-2 lg:row-start-1">
+              <svg
+                className="absolute top-0 right-0 -mt-20 -mr-20 hidden lg:block"
+                width={404}
+                height={384}
+                fill="none"
+                viewBox="0 0 404 384"
+                aria-hidden="true"
+              >
+                <defs>
+                  <pattern
+                    id="de316486-4a29-4312-bdfc-fbce2132a2c1"
+                    x={0}
+                    y={0}
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                  </pattern>
+                </defs>
+                <rect width={404} height={384} fill="url(#de316486-4a29-4312-bdfc-fbce2132a2c1)" />
+              </svg>
+              <div className="relative mx-auto max-w-prose text-base lg:max-w-none">
+                <figure>
+                  <div className="aspect-w-12 aspect-h-7 lg:aspect-none">
+                    <img
+                      className="rounded-lg object-cover object-center shadow-lg"
+                      src="https://images.unsplash.com/photo-1546913199-55e06682967e?ixlib=rb-1.2.1&auto=format&fit=crop&crop=focalpoint&fp-x=.735&fp-y=.55&w=1184&h=1376&q=80"
+                      alt="Whitney leaning against a railing on a downtown street"
+                      width={1184}
+                      height={1376}
+                    />
+                  </div>
+                  <figcaption className="mt-3 flex text-sm text-gray-500">
+                    <CameraIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+                    <span className="ml-2">Photograph by Marcus O’Leary</span>
+                  </figcaption>
+                </figure>
+              </div>
+            </div>
+            <div className="mt-8 lg:mt-0">
+              <div className="mx-auto max-w-prose text-base lg:max-w-none">
+                <p className="text-lg text-gray-500">
+                  Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique
+                  pellentesque. Blandit amet, sed aenean erat arcu morbi.
+                </p>
+              </div>
+              <div className="prose prose-indigo mx-auto mt-5 text-gray-500 lg:col-start-1 lg:row-start-1 lg:max-w-none">
+                <p>
+                  Sollicitudin tristique eros erat odio sed vitae, consequat turpis elementum. Lorem nibh vel, eget
+                  pretium arcu vitae. Eros eu viverra donec ut volutpat donec laoreet quam urna.
+                </p>
+                <p>
+                  Bibendum eu nulla feugiat justo, elit adipiscing. Ut tristique sit nisi lorem pulvinar. Urna, laoreet
+                  fusce nibh leo. Dictum et et et sit. Faucibus sed non gravida lectus dignissim imperdiet a.
+                </p>
+                <p>
+                  Dictum magnis risus phasellus vitae quam morbi. Quis lorem lorem arcu, metus, egestas netus cursus. In.
+                </p>
+                <ul role="list">
+                  <li>Quis elit egestas venenatis mattis dignissim.</li>
+                  <li>Cras cras lobortis vitae vivamus ultricies facilisis tempus.</li>
+                  <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
+                </ul>
+                <p>
+                  Rhoncus nisl, libero egestas diam fermentum dui. At quis tincidunt vel ultricies. Vulputate aliquet
+                  velit faucibus semper. Pellentesque in venenatis vestibulum consectetur nibh id. In id ut tempus
+                  egestas. Enim sit aliquam nec, a. Morbi enim fermentum lacus in. Viverra.
+                </p>
+                <h3>How we helped</h3>
+                <p>
+                  Tincidunt integer commodo, cursus etiam aliquam neque, et. Consectetur pretium in volutpat, diam.
+                  Montes, magna cursus nulla feugiat dignissim id lobortis amet. Laoreet sem est phasellus eu proin massa,
+                  lectus. Diam rutrum posuere donec ultricies non morbi. Mi a platea auctor mi.
+                </p>
+                <p>
+                  Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique
+                  pellentesque. Blandit amet, sed aenean erat arcu morbi.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="relative bg-white py-16 sm:py-24">
+        <div className="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-cols-2 lg:items-start lg:gap-24 lg:px-8">
+          <div className="relative sm:py-16 lg:py-0">
+            <div aria-hidden="true" className="hidden sm:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-screen">
+              <div className="absolute inset-y-0 right-1/2 w-full rounded-r-3xl bg-gray-50 lg:right-72" />
+              <svg
+                className="absolute top-8 left-1/2 -ml-3 lg:-right-8 lg:left-auto lg:top-12"
+                width={404}
+                height={392}
+                fill="none"
+                viewBox="0 0 404 392"
+              >
+                <defs>
+                  <pattern
+                    id="02f20b47-fd69-4224-a62a-4c9de5c763f7"
+                    x={0}
+                    y={0}
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                  </pattern>
+                </defs>
+                <rect width={404} height={392} fill="url(#02f20b47-fd69-4224-a62a-4c9de5c763f7)" />
+              </svg>
+            </div>
+            <div className="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:max-w-none lg:px-0 lg:py-20">
+              {/* Testimonial card*/}
+              <div className="relative overflow-hidden rounded-2xl pt-64 pb-10 shadow-xl">
+                <img
+                  className="absolute inset-0 h-full w-full object-cover"
+                  src="https://images.unsplash.com/photo-1521510895919-46920266ddb3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&fp-x=0.5&fp-y=0.6&fp-z=3&width=1440&height=1440&sat=-100"
+                  alt=""
+                />
+                <div className="absolute inset-0 bg-indigo-500 mix-blend-multiply" />
+                <div className="absolute inset-0 bg-gradient-to-t from-indigo-600 via-indigo-600 opacity-90" />
+                <div className="relative px-8">
+                  <div>
+                    <img
+                      className="h-12"
+                      src="https://tailwindui.com/img/logos/workcation.svg?color=white"
+                      alt="Workcation"
+                    />
+                  </div>
+                  <blockquote className="mt-8">
+                    <div className="relative text-lg font-medium text-white md:flex-grow">
+                      <svg
+                        className="absolute top-0 left-0 h-8 w-8 -translate-x-3 -translate-y-2 transform text-indigo-400"
+                        fill="currentColor"
+                        viewBox="0 0 32 32"
+                        aria-hidden="true"
+                      >
+                        <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
+                      </svg>
+                      <p className="relative">
+                        Tincidunt integer commodo, cursus etiam aliquam neque, et. Consectetur pretium in volutpat, diam.
+                        Montes, magna cursus nulla feugiat dignissim id lobortis amet.
+                      </p>
+                    </div>
+
+                    <footer className="mt-4">
+                      <p className="text-base font-semibold text-indigo-200">Sarah Williams, CEO at Workcation</p>
+                    </footer>
+                  </blockquote>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0">
+            {/* Content area */}
+            <div className="pt-12 sm:pt-16 lg:pt-20">
+              <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                On a mission to empower teams
+              </h2>
+              <div className="mt-6 space-y-6 text-gray-500">
+                <p className="text-lg">
+                  Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique
+                  pellentesque. Blandit amet, sed aenean erat arcu morbi. Cursus faucibus nunc nisl netus morbi vel
+                  porttitor vitae ut. Amet vitae fames senectus vitae.
+                </p>
+                <p className="text-base leading-7">
+                  Sollicitudin tristique eros erat odio sed vitae, consequat turpis elementum. Lorem nibh vel, eget
+                  pretium arcu vitae. Eros eu viverra donec ut volutpat donec laoreet quam urna. Sollicitudin tristique
+                  eros erat odio sed vitae, consequat turpis elementum. Lorem nibh vel, eget pretium arcu vitae. Eros eu
+                  viverra donec ut volutpat donec laoreet quam urna.
+                </p>
+                <p className="text-base leading-7">
+                  Rhoncus nisl, libero egestas diam fermentum dui. At quis tincidunt vel ultricies. Vulputate aliquet
+                  velit faucibus semper. Pellentesque in venenatis vestibulum consectetur nibh id. In id ut tempus
+                  egestas. Enim sit aliquam nec, a. Morbi enim fermentum lacus in. Viverra.
+                </p>
+              </div>
+            </div>
+
+            {/* Stats section */}
+            <div className="mt-10">
+              <dl className="grid grid-cols-2 gap-x-4 gap-y-8">
+                {stats.map((stat) => (
+                  <div key={stat.label} className="border-t-2 border-gray-100 pt-6">
+                    <dt className="text-base font-medium text-gray-500">{stat.label}</dt>
+                    <dd className="text-3xl font-bold tracking-tight text-gray-900">{stat.value}</dd>
+                  </div>
+                ))}
+              </dl>
+              <div className="mt-10">
+                <a href="#" className="text-base font-medium text-indigo-600">
+                  Learn more about how we're changing the world
+                  <span aria-hidden="true"> &rarr;</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
 
     </div>
-    
   )
 }
