6b013ff05a277f1bfb72f85ccf19bff770ea6a20
steves3142 <steves3142@gmail.com>
2022-12-08 00:37:28 -0500

    Removing excess headers

--- xophthalmics-website-1.0/pages/Buy.js
+++ xophthalmics-website-1.0/pages/Buy.js
@@ -308,49 +308,8 @@ export default function Buy() {
     <div>
       <Header />
 
-      <div className="bg-gray-50">
-        <header className="bg-indigo-600">
-          <nav className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8" aria-label="Top">
-            <div className="flex w-full items-center justify-between border-b border-indigo-500 py-6 lg:border-none">
-              <div className="flex items-center">
-                <a href="#">
-                  <span className="sr-only">Your Company</span>
-                  <img className="h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=white" alt="" />
-                </a>
-                <div className="ml-10 hidden space-x-8 lg:block">
-                  {navigation.map((item) => (
-                    <a key={item.name} href={item.href} className="text-base font-medium text-white hover:text-indigo-50">
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-              </div>
-              <div className="ml-10 space-x-4">
-                <a
-                  href="#"
-                  className="inline-block rounded-md border border-transparent bg-indigo-500 py-2 px-4 text-base font-medium text-white hover:bg-opacity-75"
-                >
-                  Sign in
-                </a>
-                <a
-                  href="#"
-                  className="inline-block rounded-md border border-transparent bg-white py-2 px-4 text-base font-medium text-indigo-600 hover:bg-indigo-50"
-                >
-                  <span className="md:hidden">Sign up</span>
-                  <span className="hidden md:inline lg:hidden">Create account</span>
-                  <span className="hidden lg:inline">Create an account</span>
-                </a>
-              </div>
-            </div>
-            <div className="mt-4 flex justify-center space-x-6 lg:hidden">
-              {navigation.map((item) => (
-                <a key={item.name} href={item.href} className="text-base font-medium text-white hover:text-indigo-50">
-                  {item.name}
-                </a>
-              ))}
-            </div>
-          </nav>
-        </header>
+      <div>
+      
 
         <main>
           {/* Pricing section */}
--- xophthalmics-website-1.0/pages/Contact.js
+++ xophthalmics-website-1.0/pages/Contact.js
@@ -100,50 +100,6 @@ export default function Contact() {
 
             <div className="bg-white">
                 <Popover as="header" className="relative">
-                    <div className="bg-warm-gray-50">
-                        <nav
-                            className="relative mx-auto flex max-w-7xl items-center justify-between px-6 pt-6 xl:px-8"
-                            aria-label="Global"
-                        >
-                            <div className="flex flex-1 items-center">
-                                <div className="flex w-full items-center justify-between lg:w-auto">
-                                    <a href="#">
-                                        <span className="sr-only">Your Company</span>
-                                        <img
-                                            className="h-8 w-auto sm:h-10"
-                                            src="https://tailwindui.com/img/logos/mark.svg?color=teal&shade=500"
-                                            alt=""
-                                        />
-                                    </a>
-                                    <div className="-mr-2 flex items-center lg:hidden">
-                                        <Popover.Button className="focus-ring-inset inline-flex items-center justify-center rounded-md bg-warm-gray-50 p-2 text-warm-gray-400 hover:bg-warm-gray-100 focus:outline-none focus:ring-2 focus:ring-teal-500">
-                                            <span className="sr-only">Open main menu</span>
-                                            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-                                        </Popover.Button>
-                                    </div>
-                                </div>
-                                <div className="hidden space-x-10 lg:ml-10 lg:flex">
-                                    {navigation.map((item) => (
-                                        <a
-                                            key={item.name}
-                                            href={item.href}
-                                            className="text-base font-medium text-warm-gray-500 hover:text-warm-gray-900"
-                                        >
-                                            {item.name}
-                                        </a>
-                                    ))}
-                                </div>
-                            </div>
-                            <div className="hidden lg:flex lg:items-center lg:space-x-6">
-                                <a
-                                    href="#"
-                                    className="rounded-md border border-transparent bg-warm-gray-100 py-2 px-6 text-base font-medium text-warm-gray-900 hover:bg-warm-gray-200"
-                                >
-                                    Login
-                                </a>
-                            </div>
-                        </nav>
-                    </div>
 
                     <Transition
                         as={Fragment}
