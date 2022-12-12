6acc67bb202e43559bd2bf7772c5bdfa72dc1494
steves3142 <steves3142@gmail.com>
2022-12-12 12:46:20 -0500

    including line breaks in calculator

--- xophthalmics-website-1.0/components/ROI_Calculator.js
+++ xophthalmics-website-1.0/components/ROI_Calculator.js
@@ -1,7 +1,21 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState, useEffect } from 'react'
+
 
 function ROI_Calculator() {
-    const [dollars, setDollars]= useState(50)
+    let [dollars, setDollars]= useState(50)
+
+    // useEffect(() => {
+    //     if (dollars > 0 && dollars < 100) {
+             
+    //     }
+    // }, [dollars]);
+
+    // const debounceAdjustVolume = useCallback(
+    //     debounce((dollars) => {
+    //         setDollars(dollars).catch((err) => {});
+    //     }, 150), 
+    //     []
+    // ); 
 
     return(
         <section className="flex-row flex-start pt-11 absolute h-96 left-3.5 right-3.5 bg-teal-400 rounded-2xl">
@@ -18,7 +32,8 @@ function ROI_Calculator() {
                             value={dollars}
                             onChange={e => setDollars(Number(e.target.value))} 
                             min={0} 
-                            max={100} />
+                            max={100} 
+                            />
                         </div>
                         <div>
 
@@ -30,10 +45,11 @@ function ROI_Calculator() {
                     <p>
                     ** The information provided here is offered by Xenon Ophthalmics. It should not be construed as tax advice or as a promise for tax savings or reduced tax liability. Individual tax situations may vary. Federal rules and tax guidelines are subject to change. Please consult with your own personal advisers.
                     </p>
-                    <break/>
+                    <br />
                     <p>
                     * This ROI calculator is provided as-is and for reference only. This is not a guarantee of your actual ROI or results. *
                     </p>
+                    <br />
                     <p>
                     * You may be eligible to write off the equipment purchase for your practice under Section 179 of the U.S. federal tax Code**. *
                     </p>
